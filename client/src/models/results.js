166d0a4de361d5ff517322c2f7c786aa868001d4
Mark Sibbald <marksibbald@me.com>
2019-01-19 15:18:39 +0000

    added results model

---
+++ client/src/models/results.js
@@ -0,0 +1,51 @@
+const RequestHelper = require('../helpers/request_helper.js')
+const PubSub = require('../helpers/pub_sub.js');
+
+const Results = function(){
+  this.items = [];
+  this.request = new RequestHelper('/app/calculator')
+}
+
+Results.prototype.setupEventsListener = function(){
+  PubSub.subscribe('FormView:formSubmit', (event) => {
+  const itemToAdd = event.detail;
+  this.add(itemToAdd);
+  })
+  PubSub.subscribe('GridView:result-delete-clicked', (event) => {
+    const itemToDelete = event.detail;
+    this.delete(itemToDelete);
+  })
+}
+
+Results.prototype.all = function(){
+  this.request
+  .get()
+  .then((listItems) => {
+    this.items = listItems;
+    PubSub.publish('Result:form-ready', this.items)
+  })
+  .catch((err) => console.error(err));
+}
+
+Results.prototype.add = function(newItem){
+  this.request
+  .post(newItem)
+  .then((listItems) => {
+    this.items = listItems;
+    PubSub.publish('Results:new-item', this.items)
+  })
+    .catch((err) => console.error(err));
+}
+
+Results.prototype.delete = function(itemToDelete){
+  const id = itemToDelete._id;
+  this.request
+  .delete(id)
+  .then((listItems) => {
+    this.items = listItems;
+    PubSub.publish('Results:deleted-item', this.items)
+  })
+      .catch((err) => console.error(err));
+}
+
+module.exports = Results;
