0c4d38931e1e3df60ee7d1cc1044743c23032d8f
Mark Sibbald <marksibbald@me.com>
2019-01-22 21:10:22 +0000

    text showing

--- client/src/views/canvas_view.js
+++ client/src/views/canvas_view.js
@@ -6,6 +6,9 @@ CanvasView.prototype.render = function () {
   if (!canvas) return;
   console.log('canvas', canvas);
   const context = canvas.getContext('2d');
+  context.font = '40pt Avenir';
+  context.textAlign = "left";
+  context.fillText('You are becoming more green!', 150, 100);
 
 
   console.log('context', context);
@@ -27,6 +30,10 @@ CanvasView.prototype.render = function () {
   canvas.addEventListener('mousemove', (event) => {
     console.log('clicked', event);
     drawImage(event.x, event.y);
+    var offset = $("#main-canvas").offset();
+startX = evt.pageX - offset.left;
+startY = evt.pageY - offset.top;
+
   })
 
   const drawImage = function(x, y){
@@ -49,12 +56,12 @@ CanvasView.prototype.render = function () {
 //   }
 // });
 
-  canvas.addEventListener('click', (event) => {
-    console.log('clicked', event);
-    drawImage(event.x, event.y);
+  // canvas.addEventListener('click', (event) => {
+  //   console.log('clicked', event);
+  //   drawImage(event.x, event.y);
 //     var posX = canvas.offsetLeft+30;
 // var posY = canvas.offsetTop+500;
-  })
+  // })
 };
 
 module.exports = CanvasView;
