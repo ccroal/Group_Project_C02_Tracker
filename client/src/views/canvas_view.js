6dda27cb1e8647388a78a7168342c4c20492dbd1
Mark Sibbald <marksibbald@me.com>
2019-01-22 20:48:08 +0000

    drawing semi working

--- client/src/views/canvas_view.js
+++ client/src/views/canvas_view.js
@@ -3,44 +3,58 @@ const CanvasView = function(){
 
 CanvasView.prototype.render = function () {
   const canvas = document.querySelector('#main-canvas');
+  if (!canvas) return;
   console.log('canvas', canvas);
   const context = canvas.getContext('2d');
 
 
-
-context.font = "14px Avenir";
-context.fillText("You are one step closer to becoming more green...", 10, 50);
-
+  console.log('context', context);
 
 
   const img = document.createElement('img');
-  img.src = "images/leaf_vector.png"
 
-  const drawCircle = function(x, y){
-  context.drawImage(img, x, y, 90, 90)
-}
+  img.src = "images/leaf-png-1.png"
+  console.log('img', img);
+  context.drawImage(img, 50, 50);
+
+
+  // const drawCircle = function(x, y){
+  //   context.beginPath();
+  //   context.arc(x, y, 50, 0, Math.PI*2, true);
+  //   context.stroke()
+  // }
+
+  canvas.addEventListener('mousemove', (event) => {
+    console.log('clicked', event);
+    drawImage(event.x, event.y);
+  })
+
+  const drawImage = function(x, y){
+    context.drawImage(img, x, y, 100, 100)
+
+  }
+//   $('#main-canvas').mousedown(function(e){
+//   var mouseX = e.pageX - this.offsetLeft;
+//   var mouseY = e.pageY - this.offsetTop;
+//
+//   paint = true;
+//   addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
+//   redraw();
+// });
+//
+// $('#main-canvas').mousemove(function(e){
+//   if(paint){
+//     addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, true);
+//     redraw();
+//   }
+// });
 
   canvas.addEventListener('click', (event) => {
     console.log('clicked', event);
-    drawCircle(event.x, event.y);
+    drawImage(event.x, event.y);
+//     var posX = canvas.offsetLeft+30;
+// var posY = canvas.offsetTop+500;
   })
-
-
-
-
-  const draw = function(){
-
-  }
-
-  img.addEventListener('load', draw);
-
-  const changeColor = function(){
-  context.strokeStyle = this.value;
-
-  }
-
-
-
 };
 
 module.exports = CanvasView;
